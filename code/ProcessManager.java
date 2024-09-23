4ed28bc713f35cbc04297922a3f2a4bfe47d44b1
rileyhart23 <riley.wasdyke@icloud.com>
2024-09-23 16:09:52 -0400

    class creating
    getting annoyed

--- code/ProcessManager.java
+++ code/ProcessManager.java
@@ -1,5 +1,11 @@
 package code;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ProcessManager {
     Process head;
 
@@ -24,7 +30,7 @@ public class ProcessManager {
                 if (temp.previous != null) {
                     temp.previous.next = temp.next;
                 } else {
-                    head = temp.next; // If head is to be deleted
+                    head = temp.next;
                 }
                 if (temp.next != null) {
                     temp.next.previous = temp.previous;
@@ -39,7 +45,7 @@ public class ProcessManager {
         Process temp = head;
 
         while (temp != null) {
-            if (temp.user.equals(user)) { // Use .equals for string comparison
+            if (temp.user == user) {
                 Process toDelete = temp;
                 temp = temp.next;
                 deleteProcessByID(toDelete.productID);
@@ -56,7 +62,37 @@ public class ProcessManager {
                     "\nName: " + temp.name +
                     "\nUser: " + temp.user +
                     "\nExecution Time: " + temp.executionTime);
-            temp = temp.next; // Move to the next node
+            temp = temp.next;
         }
     }
+
+    public void saveProcessList(String filenameToSave) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filenameToSave))) {
+            Process temp = head;
+            while (temp != null) {
+                writer.write(temp.productID + "," + temp.name + "," + temp.user + "," + temp.executionTime + "\n");
+                temp = temp.next;
+            }
+        } catch (IOException xeption) {
+            xeption.printStackTrace();
+        }
+    }
+
+    public void loadProcessList(String filenameToLoad) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filenameToLoad))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(",");
+                int productID = Integer.parseInt(data[0]);
+                String name = data[1];
+                String user = data[2];
+                int executionTime = Integer.parseInt(data[3]);
+                Process newProcess = new Process(productID, name, user, executionTime);
+                addProcess(newProcess);
+            }
+        } catch (IOException xeption) {
+            xeption.printStackTrace();
+        }
+    }
+
 }
